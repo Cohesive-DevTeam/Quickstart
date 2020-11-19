26b4d2ee22ad97bae8301f5d5e33056f0f5866f4
Cohesive Networks <50168550+Cohesive-DevTeam@users.noreply.github.com>
2020-11-19 12:50:28 +0000

    Update install options

--- CustomExtensionWindows.ps1
+++ CustomExtensionWindows.ps1
@@ -1,6 +1,8 @@
-Invoke-WebRequest -Uri https://swupdate.openvpn.org/community/releases/OpenVPN-2.5-rc2-I601-2-amd64.msi -OutFile "C:\ovpn.msi"
-Start-Process -FilePath "C:\ovpn.msi" -ArgumentList "/qn"
-Start-Sleep -s 120
+Invoke-WebRequest -Uri https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.0-I601-amd64.msi -OutFile "C:\ovpn.msi"
+
+Start-Process -FilePath "C:\ovpn.msi" -ArgumentList "ADDLOCAL=OpenVPN.Service,OpenVPN,Drivers,Drivers.Wintun /qn" 
+
+Start-Sleep -s 20
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 
